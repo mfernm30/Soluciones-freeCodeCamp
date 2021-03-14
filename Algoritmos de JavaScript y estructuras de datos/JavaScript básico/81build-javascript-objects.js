36b212dd72a2f0d628b62a173df5b665ebaa6d35
mfernm30 <mfernm30@gmail.com>
2021-03-14 19:59:01 +0100

    Añadidos ejercicios 80-111 de la sección JavaScript básico.

---
+++ Algoritmos de JavaScript y estructuras de datos/JavaScript básico/81build-javascript-objects.js
@@ -0,0 +1,9 @@
+var myDog = {
+// Only change code below this line
+"name": "Lukas",
+legs: 4,
+tails: 1,
+friends: ["Ani", "Kira"]
+
+// Only change code above this line
+};
---
+++ Algoritmos de JavaScript y estructuras de datos/JavaScript básico/90.manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add a record here
+  {
+    "artist": "Valira",
+    "title": "Supernova",
+    "release_year": 2021,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
